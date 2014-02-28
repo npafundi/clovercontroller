0efc9c8c708adc89700dae2dd47f2431fe23ee1d
Nick Pafundi <nick@cloversites.com>
2014-02-28 13:11:24 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CloverController::Application.config.secret_key_base = 'e263a807c7e29cc4e5127db37de49fb8223c4283787c7ca06863e1b69458994bc5939133baa0615f94ee23a2796d474788d3e1cc9e3fbe3181552139601a4012'
