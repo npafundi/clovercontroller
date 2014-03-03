d055d78fc2817440c0a35d6d16ff4d18a1224e18
Nick Pafundi <nick@cloversites.com>
2014-03-03 10:08:15 -0800

    Updating seeds to play a random sound of clover

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # Create a few tasks
 
 # This one should work just fine
-Task.create(name: "deploy clodo.com master [dev]", description: "Deploy cloversite.com to production.  Don't use this lightly.", request_type: 'script', action:"http://public.dev.cloverdonations.com/general-webhook.php?apiToken=#{ENV['CLOVER_API_KEY']}&hook=deploy&dest=dev&repos=cloverdonations.com")
+Task.create(name: "deploy clodo.com master [dev]", description: "Deploy cloversite.com to production", request_type: 'GET', action:"http://public.dev.cloverdonations.com/general-webhook.php?apiToken=#{ENV['CLOVER_API_KEY']}&hook=deploy&dest=dev&repos=cloverdonations.com")
 
 # FIXME - Right now, this is just for looks
-Task.create(name: "random sound", description: "Play a random sound from sounds of clover", request_type: 'GET', action:"www.google.com")
+Task.create(name: "random sound", description: "Play a random sound from sounds of clover", request_type: 'GET', action:"http://designs-mac-mini.local/ajax/random.php")
