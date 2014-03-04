41fb7cf398fe062922105cbc59e8d072e2c08172
Nick Pafundi <nick@cloversites.com>
2014-03-04 10:35:39 -0800

    Updating random sound URL

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,7 @@
 # Create a few tasks
 
-# This one should work just fine
 Task.create(name: "deploy clodo.com master [dev]", description: "Deploy cloversite.com to production", request_type: 'GET', action:"http://public.dev.cloverdonations.com/general-webhook.php?apiToken=#{ENV['CLOVER_API_KEY']}&hook=deploy&dest=dev&repos=cloverdonations.com")
 
-# FIXME - Right now, this is just for looks
-Task.create(name: "random sound", description: "Play a random sound from sounds of clover", request_type: 'GET', action:"http://designs-mac-mini.local/ajax/random.php")
+Task.create(name: "random sound", description: "Play a random sound from sounds of clover", request_type: 'GET', action:"http://cloversites.dyndns.org:20001/ajax/random.php") #internal URL: http://designs-mac-mini.local/ajax/random.php
 
 Task.create(name: "change tint gradient", description: "Update tint to a new gradient using Jason's script.", request_type: 'GET', action:"http://tint.cloversites.com/api/create_random_gradient")
