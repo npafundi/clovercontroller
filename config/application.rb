f459ae194f626336e20c4f0e526f693bc45dd004
Nick Pafundi <nick@cloversites.com>
2014-03-03 22:13:23 -0800

    wrong line...

--- config/application.rb
+++ config/application.rb
@@ -6,8 +6,6 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
-config.assets.initialize_on_precompile = false
-
 module CloverController
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -21,5 +19,6 @@ module CloverController
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.initialize_on_precompile = false
   end
 end
